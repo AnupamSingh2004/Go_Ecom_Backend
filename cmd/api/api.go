04082ef04344024b188306f78888cfddcffa4221
AnupamSingh2004 <sanupam2004@gmail.com>
2025-12-28 12:33:09 +0530

    Database connection Setup

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -2,8 +2,10 @@ package api
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 
+	"github.com/AnupamSingh2004/Go_Ecom_Backend/service/user"
 	"github.com/gorilla/mux"
 )
 
@@ -21,6 +23,12 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
+
+	log.Println("Listening on", s.addr)
 
 	return http.ListenAndServe(s.addr, router)
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/AnupamSingh2004/Go_Ecom_Backend/cmd/api"
+	"github.com/AnupamSingh2004/Go_Ecom_Backend/config"
+	"github.com/AnupamSingh2004/Go_Ecom_Backend/db"
+	"github.com/go-sql-driver/mysql"
 )
 
-func main(){
-	server := api.NewAPIServer(":8080", nil)
+func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Database Connected Successfully")
 }
